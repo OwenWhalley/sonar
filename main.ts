cc9eba4fb1136216bc36fbd9a8bf6dce399424d9
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-12-08 09:57:51 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 let distance_to_object = 0
 basic.showIcon(IconNames.Yes)
 basic.pause(500)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
 basic.forever(function () {
     distance_to_object = sonar.ping(
     DigitalPin.P1,
@@ -8,4 +11,13 @@ basic.forever(function () {
     PingUnit.Centimeters
     )
     basic.showNumber(distance_to_object)
+    if (distance_to_object > 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+    if (distance_to_object <= 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    if (distance_to_object <= 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
 })
