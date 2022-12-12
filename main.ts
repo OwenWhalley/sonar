b02067b495c003262a99cde37b4571f77524131d
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-12-12 09:41:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,44 @@
+let strip: neopixel.Strip = null
 let distance_to_object = 0
-basic.showIcon(IconNames.Yes)
-basic.pause(500)
-let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-strip.clear()
-strip.show()
 basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
     distance_to_object = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
     basic.showNumber(distance_to_object)
+    if (distance_to_object > 4) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 4) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 3) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object == 2) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
+    } else if (distance_to_object <= 1) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        strip.clear()
+    }
 })
